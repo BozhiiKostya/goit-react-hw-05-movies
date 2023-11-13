fb1524fd365f10b29cc03eee236d1c1a4e451c48
BozhiiKostya <92441917+BozhiiKostya@users.noreply.github.com>
2023-11-13 15:13:28 +0000

    Deploying to gh-pages from @ BozhiiKostya/goit-react-hw-05-movies@0ea94e03ccd8cbd2109bc9e6f465a962c5e55183 🚀

---
+++ static/js/256.3f2854d3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{256:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{style:{fontSize:20},children:"Sorry, we couldn't find that page \ud83d\ude22"})]})}}}]);
+//# sourceMappingURL=256.3f2854d3.chunk.js.map
